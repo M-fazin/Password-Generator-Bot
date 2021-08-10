98590e8a406af1b833595d3ebea418a93cd2e21f
Muhammed Fazin EK <86030159+M-fazin@users.noreply.github.com>
2021-08-10 18:49:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 Ek = Client(
     "Password Generator Bot",
     bot_token = os.environ["BOT_TOKEN"],
-    app_id = int(os.environ["API_ID"]),
+    api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
 
