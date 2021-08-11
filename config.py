996596370e528237fb0b18c33faefd8d8cd9675d
Muhammed Fazin EK <86030159+M-fazin@users.noreply.github.com>
2021-08-11 14:45:44 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # Your Bot Token From @BotFather
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-      # Your APP ID & API_HASH From my.telegram.org
-    API_HASH = os.environ.get("API_HASH")
