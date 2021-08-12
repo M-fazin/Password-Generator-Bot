e38010d8152a3d51d8ec3580950cd5c06c29fe5d
Muhammed Fazin EK <86030159+M-fazin@users.noreply.github.com>
2021-08-12 08:42:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ Ek = Client(
 
 @Ek.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
-        TEXT = f"Hai {message.from_user.mention} \n\n**I Am Password Generator Bot. I Can Generate Strong Passwords At Your Wish Length (Max. 84).** \n\nFor Know More /help"
+        TEXT = f"Hai {update.from_user.mention} \n\n**I Am Password Generator Bot. I Can Generate Strong Passwords At Your Wish Length (Max. 84).** \n\nFor Know More /help"
         BUTTONS = reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Channel 🔰", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Support Group ⭕️", url = "https://telegram.me/ekbotz_support")],[InlineKeyboardButton("Repo 🗂️", url = "https://github.com/M-fazin/Password-Generator-Bot"),InlineKeyboardButton("Deploy 🗃️", url = "https://heroku.com/deploy?template=https://github.com/M-fazin/Password-Generator-Bot")],[InlineKeyboardButton("Developer 💡", url = "https://github.com/M-fazin/")]])
         await update.reply_text(
         text=TEXT,
