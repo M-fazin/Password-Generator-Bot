617b176637de6dbe1696615d427629d52cb63b39
Muhammed Fazin EK <86030159+M-fazin@users.noreply.github.com>
2021-08-12 18:30:21 +0530

    Create main.py

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ async def start(bot, update):
 	
 @Ek.on_message(filters.private & filters.command(["help"]))
 async def help(bot, update):
-        TEXT = "Hai {} \n\n**There Is Nothing To Know More.** \n- Send Me The Limit Of Your Password \n- I Will Give The Password Of That Limit. \n\nEx:- `20` \n\n**Note :-**\n• Only Digits Are Allowed \n• Maximum Allowed Digits Till 84 (I Can't Generate Passwords Above The Length 84)"
-        BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Source Code", url = "https://github.com/M-fazin/Password-Generator-Bot")]])
+        HELP = "Hai {} \n\n**There Is Nothing To Know More.** \n- Send Me The Limit Of Your Password \n- I Will Give The Password Of That Limit. \n\nEx:- `20` \n\n**Note :-**\n• Only Digits Are Allowed \n• Maximum Allowed Digits Till 84 (I Can't Generate Passwords Above The Length 84)"
+        HELP_BUTTON = InlineKeyboardMarkup([[InlineKeyboardButton("Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Source Code", url = "https://github.com/M-fazin/Password-Generator-Bot")]])
         await update.reply_text(
-        text=TEXT.format(update.from_user.mention),
-        reply_markup=BUTTONS,
+        text=HELP.format(update.from_user.mention),
+        reply_markup=HELP_BUTTON,
         disable_web_page_preview=True,
         quote=True
         )
