bbca7f5d1810e3349a2fea4c4f3a5d97bbaf5bd5
Muhammed Fazin EK <86030159+M-fazin@users.noreply.github.com>
2021-08-12 15:31:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,38 +19,43 @@ Ek = Client(
 async def start(bot, update):
         TEXT = "Hai {} \n\n**I Am Password Generator Bot. I Can Generate Strong Passwords At Your Wish Length (Max. 84).** \n\nFor Know More /help"
         BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("Channel 🔰", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Support Group ⭕️", url = "https://telegram.me/ekbotz_support")],[InlineKeyboardButton("Repo 🗂️", url = "https://github.com/M-fazin/Password-Generator-Bot"),InlineKeyboardButton("Deploy 🗃️", url = "https://heroku.com/deploy?template=https://github.com/M-fazin/Password-Generator-Bot")],[InlineKeyboardButton("Developer 💡", url = "https://github.com/M-fazin/")]])
-        # await update.reply_text(
-        # text=TEXT,
-        # qoute=True,
-        # reply_markup=BUTTONS
-    # )
         await update.reply_text(
         text=TEXT.format(update.from_user.mention),
         reply_markup=BUTTONS,
         disable_web_page_preview=True,
         quote=True
     )
-        # await message.reply_text(TEXT, qoute=True, BUTTONS)
-	# await message.reply_text(f"Hai {message.from_user.mention} \n\n**I Am Password Generator Bot. I Can Generate Strong Passwords At Your Wish Length (Max. 84).** \n\nFor Know More /help", True , reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Support Group", url = "https://telegram.me/ekbotz_support")],[InlineKeyboardButton("Repo", url = "https://github.com/M-fazin/Password-Generator-Bot"),InlineKeyboardButton("Deploy", url = "https://heroku.com/deploy?template=https://github.com/M-fazin/Password-Generator-Bot")],[InlineKeyboardButton("Developer", url = "https://github.com/M-fazin/")]]))
 	
 @Ek.on_message(filters.private & filters.command(["help"]))
-async def help(bot, message):
-	await message.reply_text(f"Hai {message.from_user.mention} \n\n**There Is Nothing To Know More.** \n- Send Me The Limit Of Your Password \n- I Will Give The Password Of That Limit. \n\nEx:- `20` \n\n**Note :-**\n• Only Digits Are Allowed \n• Maximum Allowed Digits Till 84 (I Can't Generate Passwords Above The Length 84)", True , reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Source Code", url = "https://github.com/M-fazin/Password-Generator-Bot")]]))
+async def help(bot, update):
+        TEXT = "Hai {} \n\n**There Is Nothing To Know More.** \n- Send Me The Limit Of Your Password \n- I Will Give The Password Of That Limit. \n\nEx:- `20` \n\n**Note :-**\n• Only Digits Are Allowed \n• Maximum Allowed Digits Till 84 (I Can't Generate Passwords Above The Length 84)"
+        BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Source Code", url = "https://github.com/M-fazin/Password-Generator-Bot")]])
+	await update.reply_text(
+        text=TEXT.format(update.from_user.mention),
+        reply_markup=BUTTONS,
+        disable_web_page_preview=True,
+        quote=True
+    )
 	
 @Ek.on_message(filters.private & filters.command(["about", "source", "repo"]))
 async def about(bot, message):
- await message.reply_text(f"**• Bot :** Password Generator Bot\n\n**• Developer :** [M-fazin](https://github.com/M-fazin)\n\n**• Channel :** @EKBOTZ_UPDATE\n\n**• Support :** @ekbotz_support \n\n**• Source Code :** [Password Generator Bot](https://github.com/M-fazin/Password-Generator-Bot)\n\n**• Language :** Python 3\n\n**• Framework :** Pyrogram", quote = True, disable_web_page_preview = True)	
+        TEXT = "**• Bot :** Password Generator Bot\n\n**• Developer :** [M-fazin](https://github.com/M-fazin)\n\n**• Channel :** @EKBOTZ_UPDATE\n\n**• Support :** @ekbotz_support \n\n**• Source Code :** [Password Generator Bot](https://github.com/M-fazin/Password-Generator-Bot)\n\n**• Language :** Python 3\n\n**• Framework :** Pyrogram"
+	await update.reply_text(
+        text=TEXT,
+        disable_web_page_preview=True,
+        quote=True
+    )
 	
 @Ek.on_message(filters.private & filters.text)
 async def password(bot, message):
     password = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
     limit = int(message.text)
     if limit >= 85 or limit <= 0:
-        text = "Sorry... Limit is 1 to 84"
+        text = "Sorry... Failed To Create Password Because Limit is 1 to 84"
     else:
         randomValue = random.sample(password, limit)
         randomValue = "".join(randomValue)
-        text = f"**Your Password Generated Succesfully** 👇 :- \n `{randomValue}` \n\n**Join @EKBOTZ_UPDATE"
+        text = f"**Your Password Generated Succesfully.** Your Password Limit : `{limit}`. \nPassword 👇 :- \n`{randomValue}` \n\n**Join @EKBOTZ_UPDATE"
     await message.reply_text(text, True)
 			
 
